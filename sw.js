668fbd091198d83da55b4b76b5fe8ebd8680fbd8
bankreconciliationstatement560-create <bankreconciliationstatement560@gmail.com>
2025-12-28 12:54:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,13 +8,9 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
